941670ff81f7ee41d1ae0bfa51dc21d5628641c0
Max Base <MaxBaseCode@Gmail.Com>
2021-01-03 20:41:10 +0330

    Import libs

--- market-watch.js
+++ market-watch.js
@@ -4,3 +4,8 @@
  * @Author: Max Base
  */
 
+const fs = require('fs')
+const dotenv = require('dotenv')
+const axios = require('axios')
+// const mysql = require('promise-mysql')
+const mysql = require('mysql2/promise')
