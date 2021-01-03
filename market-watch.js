14d08dbfa51a0d6a93a22163567be5fb0a7585fa
Max Base <MaxBaseCode@Gmail.Com>
2021-01-03 20:41:34 +0330

    Adding base function and read ENV file

--- market-watch.js
+++ market-watch.js
@@ -9,3 +9,28 @@ const dotenv = require('dotenv')
 const axios = require('axios')
 // const mysql = require('promise-mysql')
 const mysql = require('mysql2/promise')
+
+const arabicToPersian = (str) => {
+  return str
+}
+
+const round = (num, dec) => {
+  const [sv, ev] = num.toString().split('e')
+  return Number(Number(Math.round(parseFloat(sv + 'e' + dec)) + 'e-' + dec) + 'e' + (ev || 0))
+}
+
+const n2n = (num) => {
+  return round(num, 2)
+}
+
+const countDigits = (input) => {
+  return 0
+}
+
+const preapreENV = () => {
+  const envConfig = dotenv.parse(fs.readFileSync('.env.txt'))
+  for (const k in envConfig) {
+    process.env[k] = envConfig[k]
+  }
+}
+preapreENV()
